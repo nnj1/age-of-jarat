cbe72b2ef1a3f55699570a31e15c28a5002709b5
shrimpboyho <shrimpboyhoftw@gmail.com>
2026-01-31 16:49:46 -0500

    need to update poppedtile

--- src/animal.gd
+++ src/animal.gd
@@ -11,3 +11,13 @@ func prepare(spawning_player_id: int = 1, given_faction: int = -1, given_lore_da
 func _ready():
 	super._ready()
 	
+func on_death():
+	
+	#spawn some meat
+	var popped_tile = preload('res://scenes/entities/objects/popped_tile.tscn').instantiate()
+	popped_tile.prepare(main_game_node.get_cropped_tile_texture(Vector2i(1,17)), 'food', randi_range(1, 5))
+	popped_tile.position = self.global_position
+	main_game_node.get_node('entities/objects').add_child(popped_tile, true)
+
+	super.on_death()
+	
